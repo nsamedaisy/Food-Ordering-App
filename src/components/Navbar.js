8f601b3dab3ba8121eb76badf880b6f9f692ea28
nsamedaisy <nsamedaisy@gmail.com>
2023-09-19 13:58:11 +0100

    added navigate to order button on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Navbar() {
-  const [openMenu, setOpenMenu] = React.useState(false)
+  
+  const navigate = useNavigate()
+  const [openMenu, setOpenMenu] = React.useState(false);
   const menuOptions = [
     {
-      text: 'Home',
-      link: '/',
+      text: "Home",
+      link: "/",
     },
     {
-      text: 'Login',
-      link: '/login',
+      text: "Login",
+      link: "/login",
     },
   ];
 
   return (
     <nav>
-      <div className='nav-logo-container'>Daisy's Ordering App</div>
-      <div className='navbar-links-container'>
+      <div className="nav-logo-container">Daisy's Ordering App</div>
+      <div className="navbar-links-container">
         {menuOptions.map((option) => (
           <Link key={option.text} to={option.link}>
             {option.text}
           </Link>
         ))}
-        <button className='primary-button'>Order Now</button>
+        <button className="primary-button" onClick={() => navigate(`/menu`)}>
+          Order Now
+        </button>
       </div>
-      <div className='navbar-menu-container'>
+      <div className="navbar-menu-container">
         <bars onClick={() => setOpenMenu(true)} />
       </div>
     </nav>
